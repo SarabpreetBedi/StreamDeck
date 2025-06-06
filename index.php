eacb8c1e7d75187da9768b2ca7afbe651a41b9c8
Sarabpreet Bedi <bedisarabpreet@gmail.com>
2025-06-06 20:07:31 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$language = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+
+// You can expand the list of supported languages if needed
+switch ($language) {
+    case "de":
+        header("Location: https://streamdeck.vivre-motion.com/de/", true, 301);
+        break;
+    case "en":
+        header("Location: https://streamdeck.vivre-motion.com/en/", true, 301);
+        break;
+    default:
+        // Default to English
+        header("Location: https://streamdeck.vivre-motion.com/en/", true, 301);
+        break;
+}
+exit;
+?>
+

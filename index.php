40e968b165ad98975f6deef6958540be7f2b01b5
SarabpreetBedi <92027008+SarabpreetBedi@users.noreply.github.com>
2025-06-08 20:20:22 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-header("Location: https://streamdeck-52o2.onrender.com/de/", true, 301);
-exit;
+// Remove the redirect if it's not needed
+// header("Location: https://streamdeck-52o2.onrender.com/de/", true, 301);
+// exit;
 ?>
-

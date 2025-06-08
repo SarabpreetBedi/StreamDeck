7023562960c318fc9de892a1a304ae228bad02cd
SarabpreetBedi <92027008+SarabpreetBedi@users.noreply.github.com>
2025-06-08 21:11:05 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
-// Remove the redirect if it's not needed
-// header("Location: https://streamdeck-52o2.onrender.com/de/", true, 301);
-// exit;
+// Get the user's language preference from the browser's Accept-Language header
+$lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+
+// Redirect to the appropriate language page based on the browser language
+if ($lang === 'de') {
+    header("Location: https://streamdeck-52o2.onrender.com/de/", true, 302);  // 302 for temporary redirection
+} else {
+    header("Location: https://streamdeck-52o2.onrender.com/en/", true, 302);  // 302 for temporary redirection
+}
+exit;
 ?>

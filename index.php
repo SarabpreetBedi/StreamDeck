32e989c3c9c25a53cf563d98874d3c2796b5208b
SarabpreetBedi <92027008+SarabpreetBedi@users.noreply.github.com>
2025-06-08 15:03:48 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,14 +4,14 @@ $language = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
 // You can expand the list of supported languages if needed
 switch ($language) {
     case "de":
-        header("Location: https://streamdeck.vivre-motion.com/de/", true, 301);
+        header("Location: https://streamdeck-52o2.onrender.com/de/", true, 301);
         break;
     case "en":
-        header("Location: https://streamdeck.vivre-motion.com/en/", true, 301);
+        header("Location: https://streamdeck-52o2.onrender.com/en/", true, 301);
         break;
     default:
         // Default to English
-        header("Location: https://streamdeck.vivre-motion.com/en/", true, 301);
+        header("Location: https://streamdeck-52o2.onrender.com/en/", true, 301);
         break;
 }
 exit;
